e37b2fc1e9cc80cd5dce9e0a8a2875a9fe65ffb9
Ahmet Emrebas <aesys+git@proton.me>
2026-08-21 00:36:34 -0500

    fix: fixed linting

--- apps/web/src/app/nx-welcome.ts
+++ apps/web/src/app/nx-welcome.ts
@@ -1,8 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-nx-welcome',
+  selector: 'ae-nx-welcome',
   imports: [CommonModule],
   template: `
     <!--
